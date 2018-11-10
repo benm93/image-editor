b38b6ec1e8fb6ef5818ee267c19ef95edfb55e1d
Benjamin Mitchell <benjaminmitchell93@gmail.com>
2018-11-10 16:03:24 -0500

    basic features

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -22,102 +22,95 @@ public class MainController {
 
 	@FXML
 	private AnchorPane ap;
-	
+
 	@FXML
 	private ImageView viewer;
-	
+
 	@FXML
-    private Button open;
-	
+	private Button open;
+
 	@FXML
-    private Label path;
-	
+	private Label path;
+
 	@FXML
-    private Slider brightnessSlider;
-	
+	private Slider brightnessSlider;
+
 	@FXML
-    private Slider contrastSlider;
-	
+	private Slider contrastSlider;
+
 	@FXML
-    private Slider saturationSlider;
-	
+	private Slider saturationSlider;
+
 	@FXML
-    private Button save;
-	
+	private Button save;
+
 	@FXML
-    void saturationChange(MouseEvent event) {
-		double saturation = saturationSlider.getValue() / 500;
+	void saturationChange(MouseEvent event) {
+		double saturation = saturationSlider.getValue() / 400;
 		path.setText(Double.toString(saturation));
 		ColorAdjust colorAdjust = new ColorAdjust();
 		colorAdjust.setSaturation(saturation);
 		viewer.setEffect(colorAdjust);
 	}
-	
+
 	@FXML
-    void saveFile(MouseEvent event) {
+	void saveFile(MouseEvent event) {
 		path.setText("clicked on save");
 		FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Save Image");        
-        File file = fileChooser.showSaveDialog(ap.getScene().getWindow());
-        if (file != null) {
-            try {
-                //ImageIO.write(SwingFXUtils.fromFXImage(viewer.getImage(),
-            	ImageIO.write(SwingFXUtils.fromFXImage(viewer.getImage(),
-                        null), "png", file);
-            } catch (IOException ex) {
-                System.out.println("nope");
-            }
-        }
-    }
-	
+		fileChooser.setTitle("Save Image");
+		File file = fileChooser.showSaveDialog(ap.getScene().getWindow());
+		if (file != null) {
+			try {
+				// ImageIO.write(SwingFXUtils.fromFXImage(viewer.getImage(),
+				ImageIO.write(SwingFXUtils.fromFXImage(viewer.getImage(), null), "png", file);
+			} catch (IOException ex) {
+				System.out.println("nope");
+			}
+		}
+	}
+
 	@FXML
-    void contrastChange(MouseEvent event) {
-		double contrast = contrastSlider.getValue() / 2000;
+	void contrastChange(MouseEvent event) {
+		double contrast = contrastSlider.getValue() / 800;
 		path.setText(Double.toString(contrast));
 		ColorAdjust colorAdjust = new ColorAdjust();
 		colorAdjust.setContrast(contrast);
 		viewer.setEffect(colorAdjust);
 	}
-	
+
 	@FXML
-    void sliderChange(MouseEvent event) {
-		double brightness = brightnessSlider.getValue() / 100;		
+	void sliderChange(MouseEvent event) {
+		double brightness = brightnessSlider.getValue() / 100;
 		path.setText(Double.toString(brightness));
-		//Image image = viewer.getImage();
-		//PixelReader pixelReader = image.getPixelReader();
+		// Image image = viewer.getImage();
+		// PixelReader pixelReader = image.getPixelReader();
 		ColorAdjust colorAdjust = new ColorAdjust();
-		//colorAdjust.setContrast(0.1);
-		//colorAdjust.setHue(-0.05);
+		// colorAdjust.setContrast(0.1);
+		// colorAdjust.setHue(-0.05);
 		colorAdjust.setBrightness(brightness);
-		//colorAdjust.setSaturation(0.2);
+		// colorAdjust.setSaturation(0.2);
 		viewer.setEffect(colorAdjust);
 		/*
-		for (int readY = 0; readY < image.getHeight(); readY++) {
-            for (int readX = 0; readX < image.getWidth(); readX++) {
-                Color color = pixelReader.getColor(readX, readY);
-                System.out.println("\nPixel color at coordinates ("
-                        + readX + "," + readY + ") "
-                        + color.toString());
-                System.out.println("R = " + color.getRed());
-                System.out.println("G = " + color.getGreen());
-                System.out.println("B = " + color.getBlue());
-                System.out.println("Opacity = " + color.getOpacity());
-                System.out.println("Saturation = " + color.getSaturation());
-            }
-        }
-        */
-	}    
-    
-    @FXML
-    void openFile(MouseEvent event) {
-    	
-    	FileChooser chooser = new FileChooser();
-        chooser.setTitle("Open File");
-        File img = chooser.showOpenDialog(ap.getScene().getWindow());
-        String name = "file:" + img.getAbsolutePath();
-        path.setText(name);
-        Image image = new Image(name);        
-        viewer.setImage(image);
-    }
+		 * for (int readY = 0; readY < image.getHeight(); readY++) { for (int readX = 0;
+		 * readX < image.getWidth(); readX++) { Color color =
+		 * pixelReader.getColor(readX, readY);
+		 * System.out.println("\nPixel color at coordinates (" + readX + "," + readY +
+		 * ") " + color.toString()); System.out.println("R = " + color.getRed());
+		 * System.out.println("G = " + color.getGreen()); System.out.println("B = " +
+		 * color.getBlue()); System.out.println("Opacity = " + color.getOpacity());
+		 * System.out.println("Saturation = " + color.getSaturation()); } }
+		 */
+	}
 
+	@FXML
+	void openFile(MouseEvent event) {
+
+		FileChooser chooser = new FileChooser();
+		chooser.setTitle("Open File");
+		File img = chooser.showOpenDialog(ap.getScene().getWindow());
+		String name = "file:" + img.getAbsolutePath();
+		path.setText(name);
+		Image image = new Image(name);
+		viewer.setImage(image);
+	}
 }
