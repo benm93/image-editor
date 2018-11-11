72a4c8d8372fe6aa49cd930c5ac513c65bddad6a
Benjamin Mitchell <benjaminmitchell93@gmail.com>
2018-11-10 20:16:05 -0500

    double click to reset sliders

--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 * Open image
 * Edit with brightness, contrast, saturation sliders
 * Save edited image
+* Slider reset on double click
 
 ## Backlog:
 
 * Create a histogram
 * Create presets feature
-* Slider reset on double click
 * Filter files in opener to show only images
-
+* Layering and selection tools (brush, magic wand)
--- src/application/MainController.java
+++ src/application/MainController.java
@@ -6,6 +6,9 @@ import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.collections.ObservableMap;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -14,6 +17,7 @@ import javafx.scene.control.Slider;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
@@ -44,15 +48,109 @@ public class MainController {
 	@FXML
 	private Button save;
 
+	private double saturationValue;
+
+	private double contrastValue;
+
+	private double brightnessValue;
+
+	public void setSaturationValue(double saturationValue) {
+		this.saturationValue = saturationValue;
+		updateSettings();
+	}
+
+	public void setContrastValue(double contrastValue) {
+		this.contrastValue = contrastValue;
+		updateSettings();
+	}
+
+	public void setBrightnessValue(double brightnessValue) {
+		this.brightnessValue = brightnessValue;
+		updateSettings();
+	}
+
+	private void updateSettings() {
+		ColorAdjust colorAdjust = new ColorAdjust();
+		colorAdjust.setContrast(contrastValue);
+		colorAdjust.setSaturation(saturationValue);
+		colorAdjust.setBrightness(brightnessValue);
+		viewer.setEffect(colorAdjust);
+	}
+	
+	@FXML
+	void resetBrightnessSlider(MouseEvent event) {
+		if (event.getClickCount() == 2) {
+			brightnessSlider.setValue(0);
+			this.setBrightnessValue(0);
+		}
+	}
+
+	@FXML
+	void resetContrastSlider(MouseEvent event) {
+		if (event.getClickCount() == 2) {
+			contrastSlider.setValue(0);
+			this.setContrastValue(0);
+		}
+	}
+
+	@FXML
+	void resetSatSlider(MouseEvent event) {
+		if (event.getClickCount() == 2) {
+			saturationSlider.setValue(0);
+			this.setSaturationValue(0);
+		}
+	}
+
 	@FXML
 	void saturationChange(MouseEvent event) {
 		double saturation = saturationSlider.getValue() / 400;
-		path.setText(Double.toString(saturation));
-		ColorAdjust colorAdjust = new ColorAdjust();
-		colorAdjust.setSaturation(saturation);
-		viewer.setEffect(colorAdjust);
+		this.setSaturationValue(saturation);
+	}
+	
+	@FXML
+	void contrastChange(MouseEvent event) {
+		double contrast = contrastSlider.getValue() / 800;
+		this.setContrastValue(contrast);
 	}
 
+	@FXML
+	void sliderChange(MouseEvent event) {
+		double brightness = brightnessSlider.getValue() / 100;
+		this.setBrightnessValue(brightness);
+	}
+
+	@FXML
+	void openFile(MouseEvent event) {
+		FileChooser chooser = new FileChooser();
+		chooser.setTitle("Open File");
+		File img = chooser.showOpenDialog(ap.getScene().getWindow());
+		String name = "file:" + img.getAbsolutePath();
+		path.setText(name);
+		Image image = new Image(name);
+		viewer.setImage(image);
+
+		if (image != null) {
+			double w = 0;
+			double h = 0;
+
+			double ratioX = viewer.getFitWidth() / image.getWidth();
+			double ratioY = viewer.getFitHeight() / image.getHeight();
+
+			double reducCoeff = 0;
+			if (ratioX >= ratioY) {
+				reducCoeff = ratioY;
+			} else {
+				reducCoeff = ratioX;
+			}
+
+			w = image.getWidth() * reducCoeff;
+			h = image.getHeight() * reducCoeff;
+
+			viewer.setX((viewer.getFitWidth() - w) / 2);
+			viewer.setY((viewer.getFitHeight() - h) / 2);
+		}
+	}
+	
 	@FXML
 	void saveFile(MouseEvent event) {
 		path.setText("clicked on save");
@@ -67,50 +165,5 @@ public class MainController {
 				System.out.println("nope");
 			}
 		}
-	}
-
-	@FXML
-	void contrastChange(MouseEvent event) {
-		double contrast = contrastSlider.getValue() / 800;
-		path.setText(Double.toString(contrast));
-		ColorAdjust colorAdjust = new ColorAdjust();
-		colorAdjust.setContrast(contrast);
-		viewer.setEffect(colorAdjust);
-	}
-
-	@FXML
-	void sliderChange(MouseEvent event) {
-		double brightness = brightnessSlider.getValue() / 100;
-		path.setText(Double.toString(brightness));
-		// Image image = viewer.getImage();
-		// PixelReader pixelReader = image.getPixelReader();
-		ColorAdjust colorAdjust = new ColorAdjust();
-		// colorAdjust.setContrast(0.1);
-		// colorAdjust.setHue(-0.05);
-		colorAdjust.setBrightness(brightness);
-		// colorAdjust.setSaturation(0.2);
-		viewer.setEffect(colorAdjust);
-		/*
-		 * for (int readY = 0; readY < image.getHeight(); readY++) { for (int readX = 0;
-		 * readX < image.getWidth(); readX++) { Color color =
-		 * pixelReader.getColor(readX, readY);
-		 * System.out.println("\nPixel color at coordinates (" + readX + "," + readY +
-		 * ") " + color.toString()); System.out.println("R = " + color.getRed());
-		 * System.out.println("G = " + color.getGreen()); System.out.println("B = " +
-		 * color.getBlue()); System.out.println("Opacity = " + color.getOpacity());
-		 * System.out.println("Saturation = " + color.getSaturation()); } }
-		 */
-	}
-
-	@FXML
-	void openFile(MouseEvent event) {
-
-		FileChooser chooser = new FileChooser();
-		chooser.setTitle("Open File");
-		File img = chooser.showOpenDialog(ap.getScene().getWindow());
-		String name = "file:" + img.getAbsolutePath();
-		path.setText(name);
-		Image image = new Image(name);
-		viewer.setImage(image);
 	}
 }
